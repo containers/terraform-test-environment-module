3069fab3aecc6284b070cb0c93faca299e1212fa
Liora Milbaum <lmilbaum@redhat.com>
2024-02-01 11:38:59 +0200

    Initial Commit

---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "AWS region."
+  type        = string
+}
+
+variable "aws_profile" {
+  description = "AWS profile."
+  type        = string
+  default     = "default"
+}
