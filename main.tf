92aa9ed218f8965882d9d3c58cebb94794ac4bab
Liora Milbaum <lmilbaum@redhat.com>
2024-02-04 07:52:22 +0200

    VPC and environment variable

--- main.tf
+++ main.tf
@@ -1,4 +1,15 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.2"
+
+  name     = "vpc-${var.environment}"
+  vpc_cidr = "10.0.0.0/16"
+  azs      = slice(data.aws_availability_zones.available.names, 0, 3)
+}
+
 module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.6.0"
+
+  name   = "i-${var.environment}"
 }
