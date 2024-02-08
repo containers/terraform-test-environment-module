66cf0d56fbb00144f9409f206873243b9da94c98
Liora Milbaum <lmilbaum@redhat.com>
2024-02-08 07:30:48 +0200

    Limit to the first az

--- main.tf
+++ main.tf
@@ -6,12 +6,14 @@ module "vpc" {
 
   name = "vpc-${var.environment}"
   cidr = "10.0.0.0/16"
-  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+  azs  = element(data.aws_availability_zones.available.names, 0)
 }
 
 module "ec2-instance" {
   source  = "terraform-aws-modules/ec2-instance/aws"
   version = "5.6.0"
 
-  name = "i-${var.environment}"
+  name              = "i-${var.environment}"
+  availability_zone = element(module.vpc.azs, 0)
+  subnet_id         = element(module.vpc.public_subnets, 0)
 }
