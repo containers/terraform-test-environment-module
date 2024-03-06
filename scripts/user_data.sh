3df8f1df6a6d68328751539a0cf25e2d087f39cc
Liora Milbaum <lmilbaum@redhat.com>
2024-03-06 12:23:56 +0200

    Running the user_data script as root user

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -1,16 +1,17 @@
 #!/usr/bin/env bash
 
+dnf install -y flatpak xorg-x11-xauth xorg-x11-xdm
+dnf clean all
+echo "X11Forwarding yes" >> /etc/ssh/sshd_config
+echo "X11DisplayOffset 10" >> /etc/ssh/sshd_config
+echo "X11UseLocalhost yes" >> /etc/ssh/sshd_config
+systemctl restart sshd
+
 sudo -u fedora -i <<'EOF'
 
-sudo dnf install -y flatpak xorg-x11-xauth xorg-x11-xdm
-sudo dnf clean all
 touch ~/.Xauthority
 flatpak update
 flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak install -y --user flathub io.podman_desktop.PodmanDesktop
-sudo echo "X11Forwarding yes" >> /etc/ssh/sshd_config
-sudo echo "X11DisplayOffset 10" >> /etc/ssh/sshd_config
-sudo echo "X11UseLocalhost yes" >> /etc/ssh/sshd_config
-sudo systemctl restart sshd
 
 EOF
