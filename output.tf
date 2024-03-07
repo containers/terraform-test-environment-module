88ca3e7ceec3ff980ee19c555588d99edfe2d7f2
Liora Milbaum <lmilbaum@redhat.com>
2024-03-07 12:35:55 +0200

    chatbot AI workflow

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+locals {
+  pem_filename = basename(local_sensitive_file.private_key.filename)
+}
+
 output "ssh_connect" {
-  value = "ssh -X -i ${local_sensitive_file.private_key.filename} fedora@${module.ec2-instance.public_dns}"
+  value = "ssh -i ${local.pem_filename} fedora@${module.ec2-instance.public_dns}"
 }
