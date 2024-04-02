e588006dd14579e0c943d051734622fc46f7c34a
Liora Milbaum <lmilbaum@redhat.com>
2024-04-03 02:26:43 +0300

    output private key contnet
    Signed-off-by: Liora Milbaum <lmilbaum@redhat.com>

--- output.tf
+++ output.tf
@@ -3,9 +3,9 @@ locals {
 }
 
 output "host" {
-  value = "${module.ec2-instance.public_dns}"
+  value = ${module.ec2-instance.public_dns}
 }
 
-output "key_path" {
-  value = "${local.pem_filename}"
+output "key" {
+  value = ${module.key_pair.private_key_openssh}
 }
