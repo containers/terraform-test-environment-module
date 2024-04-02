0a8637b8db16f6ce3d64d12ca4dd54577a6a3224
Liora Milbaum <lmilbaum@redhat.com>
2024-04-03 01:13:28 +0300

    provisioning migrated to the workflow
    Signed-off-by: Liora Milbaum <lmilbaum@redhat.com>

--- output.tf
+++ output.tf
@@ -2,6 +2,10 @@ locals {
   pem_filename = basename(local_sensitive_file.private_key.filename)
 }
 
-output "ssh_connect" {
-  value = "ssh -i ${local.pem_filename} fedora@${module.ec2-instance.public_dns}"
+output "host" {
+  value = "${module.ec2-instance.public_dns}"
+}
+
+output "key_path" {
+  value = "${local.pem_filename}"
 }
