8038d1157c4eb3ebd973afe659f6bd47cbb3f9b0
Liora Milbaum <lmilbaum@redhat.com>
2024-04-02 21:27:12 +0300

    AWS auth
    Signed-off-by: Liora Milbaum <lmilbaum@redhat.com>

--- providers.tf
+++ providers.tf
@@ -1,6 +1,7 @@
 provider "aws" {
-  region  = var.aws_region
-  profile = var.aws_profile
+  region     = var.aws_region
+  access_key = var.access_key
+  secret_key = var.secret_key
 }
 
 provider "tls" {}
