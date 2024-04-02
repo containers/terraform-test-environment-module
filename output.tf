82f2e3f9abbc44644f5797c27643e1796017c44c
Liora Milbaum <lmilbaum@redhat.com>
2024-04-03 02:36:28 +0300

    ouputs both key and key_path
    Signed-off-by: Liora Milbaum <lmilbaum@redhat.com>

--- output.tf
+++ output.tf
@@ -6,6 +6,11 @@ output "host" {
   value = "${module.ec2-instance.public_dns}"
 }
 
+output "key_path" {
+  value = "${local.pem_filename}"
+}
+
 output "key" {
-  value = "${module.key_pair.private_key_openssh}"
+  value     = "${module.key_pair.private_key_openssh}"
+  sensitive = true
 }
