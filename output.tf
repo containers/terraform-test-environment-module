f096740dbae849312c240119439c44c4f9141593
Liora Milbaum <lmilbaum@redhat.com>
2024-05-02 18:48:04 +0300

    Update output.tf - ssh_public_key

--- output.tf
+++ output.tf
@@ -10,6 +10,10 @@ output "host" {
   value = "${module.ec2-instance.public_dns}"
 }
 
+output "ssh_public_key" {
+  value = "${module.key_pair.public_key_openssh}"
+}
+
 output "pem_filename" {
   value = "${local.pem_filename}"
 }
